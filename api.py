ff944873b6213333cf807f4e206360568a306216
Oscar Byrne <oscar.byrne@gmail.com>
2018-07-26 13:22:18 -0700

    abandon OO redesign; clean up API for generators

--- api.py
+++ api.py
@@ -1,93 +1,85 @@
 from itertools import (
+    tee,
     starmap,
+    islice,
+    cycle,
+    chain,
     combinations_with_replacement,
 )
+from functools import partial
 from fractions import Fraction
 
 
 chromatic_cardinality = 12
 consonance_vector = (1,0,0,1,1,1,0,1,1,1,0,0)
-chromatic_set = set(
-    range(chromatic_cardinality)
-)
 
 
-# Types
-
-def unordered_pitch_class_set(pitches):
-    return set(
-        p % chromatic_cardinality for p in pitches
-    )
-
-def ordered_pitch_class_set(pitches):
-    return sorted(
-        unordered_pitch_class_set(pitches)
-    )
 
 def directed_pitch_interval_class(pitch1, pitch2):
     return (pitch2 - pitch1) % chromatic_cardinality
 
-
-# Unordered PCS transforms
-
-def inversion(pitches, n=chromatic_cardinality):
-    return type(pitches)(
-        n - p for p in pitches
-    )
-
-def transposition(pitches, n):
-    return type(pitches)(
-        p + n for p in pitches
+def undirected_pitch_interval_class(pitch1, pitch2):
+    return min(
+        directed_pitch_interval_class(pitch1, pitch2),
+        directed_pitch_interval_class(pitch2, pitch1)
     )
 
 
-#  Ordered PCS transforms
+def pitch_class(pitch):
+    return pitch % chromatic_cardinality
 
-def rotation(pitches, n):
-    return tuple(
-        pitches[n:] + pitches[:n]
-    )
+def inverted(pitch, n=chromatic_cardinality):
+    return n - pitch
 
-def stepwise_intervals(pitches):
-    return tuple(starmap(
-        directed_pitch_interval_class,
-        zip(
-            pitches,
-            rotation(pitches, 1)
-        )
+def transposed(pitch, n):
+    return pitch + n
+
+
+def rotated(pitches, n):
+    return tuple(chain(
+        islice(pitches, n, None),
+        islice(pitches, n)
     ))
 
-def scale_intervals(pitches, root=None):
+def stepwise_intervals(pitches):
+    a, b = tee(pitches)
+    b = islice(cycle(b), 1, None)
+    return tuple(starmap(
+        directed_pitch_interval_class,
+        zip(a, b)
+    ))
+
+def relative_intervals(pitches, root=None):
     if root is None:
-        root = pitches[0]
-    return ordered_pitch_class_set(
-        transposition(pitches, -root)
-    )
-
-
+        pitches, copy = tee(pitches)
+        root = next(copy)
+    return tuple(map(
+        partial(directed_pitch_interval_class, root),
+        pitches
+    ))
 
 
 def normal_order(pitches):
-    pcs = ordered_pitch_class_set(pitches)
+    pcs = sorted(set(
+        map(pitch_class, pitches)
+    ))
     ics = stepwise_intervals(pcs)
-    candidates = [
-        rotation(pcs, i + 1)
-        for i, x in enumerate(ics) if x == max(ics)
-    ]
+    candidates = tuple(
+        rotated(pcs, n + 1)
+        for n, ic in enumerate(ics) if ic == max(ics)
+    )
     return min(
         candidates,
-        key=scale_intervals
+        key=relative_intervals
     )
 
 def prime_form(pitches):
     nf1 = normal_order(pitches)
-    nf2 = normal_order(inversion(nf1))
+    nf2 = normal_order(map(inverted, nf1))
     return min(
         nf1, nf2,
-        key=scale_intervals
+        key=relative_intervals
     )
-
-
 
 
 
@@ -102,12 +94,14 @@ def idiomatic_consonance(pitches):
         len(pairs)
     )
 
-def idiomatically_consonant_pitch_classes(pitches, select_from=chromatic_set):
-    pcs = unordered_pitch_class_set(pitches)
+def idiomatically_consonant_pitch_classes(pitches, select_from=None):
+    if select_from is None:
+        select_from = range(chromatic_cardinality)
+    pitches = set(pitches)
     return {
         pitch for pitch in select_from
         if idiomatic_consonance(
-            set.union(pcs, {pitch})
+            set.union(pitches, {pitch})
         ) == 1
     }
 
@@ -142,12 +136,12 @@ def idiomatically_consonant_subsets(pitches):
 
 def general_chord_type(pitches):
     
-    consonant_sets = idiomatically_consonant_subsets(pitches)    
+    consonant_sets = idiomatically_consonant_subsets(pitches)
 
     max_length = len(
         max(consonant_sets, key=len)
     )
-
+    
     base_sets = tuple(
         normal_order(pcs) for pcs in consonant_sets if len(pcs) == max_length
     )
@@ -156,20 +150,15 @@ def general_chord_type(pitches):
         normal_order(set(pitches) - set(base_set)) for base_set in base_sets
     )
 
-    gcts = zip(base_sets, extensions)
+    gcts = tuple(
+        zip(base_sets, extensions)
+    )
 
     for i, gct in enumerate(gcts):
         base, exts = gct
         root = base[0]
-        base = scale_intervals(base, root)
-        exts = scale_intervals(exts, root)
+        base = relative_intervals(base, root)
+        exts = relative_intervals(exts, root)
         gcts[i] = (root, base, exts)
 
     return tuple(gcts)
-
-
-
-
-
-
-
