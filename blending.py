1a09c15a5fb7b3a0d997af950abf40138b4c7e52
Oscar Byrne <oscar.byrne@gmail.com>
2018-08-01 19:55:04 -0700

    tidy up blending code

--- blending.py
+++ blending.py
@@ -27,19 +27,18 @@ class RelNote(Axiom):
     pass
 
 
-def get_chord_spec(notes, root):
+def get_chord_spec(notes):
+    chord = Chord.classify_uniquely(notes)
     return {
-        *map(AbsNote, notes),
-        *map(RelNote, relative_intervals(notes, root))
+        *map(AbsNote, chord),
+        *map(RelNote, relative_intervals(chord, chord.root))
     }
 
-def check_for_consonance(axioms):
-    forbidden = (
-        {RelNote(3), RelNote(4)},
-        {RelNote(1)},
-        {RelNote(6), RelNote(7)},
-    )
-    return not any(subset <= axioms for subset in forbidden)
+def get_generalization_path(spec, generic_space):
+    signifying_axioms = set.difference(spec, generic_space)
+    rank = rank_axiom_salience(signifying_axioms)
+    # TODO: account for deviations
+    return sorted(signifying_axioms, key=lambda axiom: rank[axiom])
 
 def rank_axiom_salience(axioms):
     ranked = {}
@@ -51,23 +50,24 @@ def rank_axiom_salience(axioms):
         ranked[axiom] = rank
     return ranked
 
-def get_generalization_path(spec, generic_space):
-    signifying_axioms = set.difference(spec, generic_space)
-    rank = rank_axiom_salience(signifying_axioms)
-    # TODO: account for deviations
-    return sorted(signifying_axioms, key=lambda axiom: rank[axiom])
-
 def generalized(spec, generalization_path, prefix):
     return {
         axiom for axiom in spec if axiom not in generalization_path[:prefix]
     }
 
-def blend_chords(pc1, pc2):
-    chord1 = Chord.classify_uniquely(pc1)
-    chord2 = Chord.classify_uniquely(pc2)
+def check_for_consistency(blendoid):
+    forbidden = (
+        {RelNote(3), RelNote(4)},
+        {RelNote(1)},
+        {RelNote(6), RelNote(7)},
+    )
+    axioms = get_chord_spec(blendoid)
+    return not any(subset <= axioms for subset in forbidden)
 
-    spec1 = get_chord_spec(chord1, chord1.root)
-    spec2 = get_chord_spec(chord2, chord2.root)
+def blend_chords(pc1, pc2):
+
+    spec1 = get_chord_spec(pc1)
+    spec2 = get_chord_spec(pc2)
 
     generic_space = set.intersection(spec1, spec2)
 
@@ -78,34 +78,25 @@ def blend_chords(pc1, pc2):
 
     for i, _ in enumerate(path1):
         for j, _ in enumerate(path2):
-
-            generalized1 = generalized(spec1, path1, i)
-            generalized2 = generalized(spec2, path2, j)
             
-            # TODO: is this colimit operator correct?
-            colimit = set.union(generalized1, generalized2)
+            colimit = set.union(
+                generalized(spec1, path1, i),
+                generalized(spec2, path2, j)
+            )
 
             deduced  = Chord.classify_uniquely(
                 pitch_class(axiom) for axiom in colimit if type(axiom) is AbsNote
             )
 
-            additional = (
-                pitch_class(axiom + deduced.root) for axiom in colimit if type(axiom) is RelNote
-            )
+            blendoid = {
+                *deduced,
+                *(pitch_class(axiom + deduced.root) for axiom in colimit if type(axiom) is RelNote)
+            }
 
-            completed = Chord.classify_uniquely(
-                {*deduced, *additional}
-            )
-
-            spec = get_chord_spec(
-                completed,
-                completed.root
-            )
-
-            if deduced.root == completed.root and check_for_consonance(spec):
+            if check_for_consistency(blendoid):
                 # TODO: account for deviations from priority ordering
                 cost = max(i, j) ** 2 + min(i, j)
-                solutions.append((completed, cost))
+                solutions.append((blendoid, cost))
 
     return min(
         solutions,
